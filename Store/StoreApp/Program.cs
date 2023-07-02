3e74503d0886e3f7adf81da85ef112d559892c9a
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-07-02 13:30:36 +0300

    migrations

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();//servis kaydı
+ 
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));//veri tabanı bağlantı servisi
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute("default",
+"{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
